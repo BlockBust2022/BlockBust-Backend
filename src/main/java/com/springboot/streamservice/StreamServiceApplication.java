63045b672304288f8adad9926a849209c7970dd1
Shubham Deshmukh <dshubham1295@gmail.com>
2021-11-01 20:28:54 +0530

    Initial Commit

--- src/main/java/com/springboot/streamservice/StreamServiceApplication.java
+++ src/main/java/com/springboot/streamservice/StreamServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableEurekaClient
 public class StreamServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StreamServiceApplication.class, args);
+    }
 
 }
