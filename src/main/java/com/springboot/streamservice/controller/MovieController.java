96dc12e22d78b266db97d5f719e50c8250fb530a
Shubham Deshmukh <dshubham1295@gmail.com>
2021-11-02 01:14:12 +0530

    Cross origin issue

--- src/main/java/com/springboot/streamservice/controller/MovieController.java
+++ src/main/java/com/springboot/streamservice/controller/MovieController.java
@@ -1,16 +1,13 @@
 package com.springboot.streamservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.springboot.streamservice.service.MovieService;
 
 @RestController
 @RequestMapping("/movie")
+@CrossOrigin(origins = "*")
 public class MovieController {
 
 	@Autowired
