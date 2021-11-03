6536cf31240f09648912709588b52ca6a97f7d3c
Shubham Deshmukh <dshubham1295@gmail.com>
2021-11-03 21:49:33 +0530

    Revert "Revert "Fixed Vid Cloud Url""
    This reverts commit 0eb96e38ec089960710e76ee207366e31a4a3dbb.

--- src/main/java/com/springboot/streamservice/bean/tmbdbean/Episode.java
+++ src/main/java/com/springboot/streamservice/bean/tmbdbean/Episode.java
@@ -8,4 +8,69 @@ public class Episode {
     public String overview;
     public int season_number;
     public String still_path;
+    public String url;
+
+    public String getAir_date() {
+        return air_date;
+    }
+
+    public void setAir_date(String air_date) {
+        this.air_date = air_date;
+    }
+
+    public int getEpisode_number() {
+        return episode_number;
+    }
+
+    public void setEpisode_number(int episode_number) {
+        this.episode_number = episode_number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public int getSeason_number() {
+        return season_number;
+    }
+
+    public void setSeason_number(int season_number) {
+        this.season_number = season_number;
+    }
+
+    public String getStill_path() {
+        return still_path;
+    }
+
+    public void setStill_path(String still_path) {
+        this.still_path = still_path;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
