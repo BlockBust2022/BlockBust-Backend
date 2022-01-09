cf6c0b5a46fff5cd1fd9d34bec5e97def10a32cf
Shubham Deshmukh <dshubham1295@gmail.com>
2022-01-09 14:55:53 +0530

    Fix Search Bug

--- src/main/java/com/springboot/streamservice/bean/SearchResponse.java
+++ src/main/java/com/springboot/streamservice/bean/SearchResponse.java
@@ -9,4 +9,36 @@ public class SearchResponse {
 	public List<Result> results;
 	public int total_pages;
 	public int total_results;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public List<Result> getResults() {
+		return results;
+	}
+
+	public void setResults(List<Result> results) {
+		this.results = results;
+	}
+
+	public int getTotal_pages() {
+		return total_pages;
+	}
+
+	public void setTotal_pages(int total_pages) {
+		this.total_pages = total_pages;
+	}
+
+	public int getTotal_results() {
+		return total_results;
+	}
+
+	public void setTotal_results(int total_results) {
+		this.total_results = total_results;
+	}
 }
